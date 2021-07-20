3513bb9c7a4b9fb7604de529a46d6c4db2df9efb
Irena Chen <irena1009@gmail.com>
2021-07-19 22:11:58 -0700

    revise Irena component and add css

--- src/Irena.js
+++ src/Irena.js
@@ -6,22 +6,31 @@ function Irena(){
         document.querySelector(".frame1").style.top = "40vh"
         document.querySelector(".frame1").style.height = "6rem"
         document.querySelector(".frame2").style.left = "110vw"
-        document.querySelector(".Irena").style.width = "250px"
+        document.querySelector(".Irena").style.width = "251px"
         document.querySelector(".Irena").style.height = "6rem"
         document.querySelector(".Irena").style.top = "40vh"
-        document.querySelector(".Irena").style.left = "43vw"
+        document.querySelector(".Irena").style.left = `${(window.innerWidth-251)/2}px`
+        console.log(typeof(window.innerWidth))
         document.querySelector(".Irena").style["line-height"] = "6rem"
-        // document.querySelector(".letter-group-EN").style.left = document.querySelector(".letter-group-IR").clientWidth
+
+        const irWidth = document.querySelector(".letter-group-IR").getBoundingClientRect().width
+
+        document.querySelector(".letter-group-EN").style.left = `${irWidth}px`
         document.querySelector(".letter-group-EN").style.top = "0%"
         document.querySelector(".letter-group-EN").style["letter-spacing"] = "8px"
+        const enWidth = document.querySelector(".letter-group-EN").getBoundingClientRect().width
+
         document.querySelector(".letter-group-A").style["letter-spacing"] = "8px"
+        document.querySelector(".letter-group-A").style.left = `${irWidth + enWidth - 27}px`
         document.querySelector(".letter-group-A").style.top = "0%"
+        const aWidth = document.querySelector(".letter-group-A").getBoundingClientRect().width
+
+        document.querySelector(".letter-group-C").style.left = `${irWidth + enWidth + aWidth -36 + 12}px`
         document.querySelector(".letter-group-C").style.top = "0%"
-        // document.querySelectorAll("span").forEach(ele => ele.style.position = "relative")
     }
 
-    // useEffect(()=> setTimeout(moveGrid, 500)
-    // ,[]);
+    useEffect(()=> setTimeout(moveGrid, 500)
+    ,[]);
 
     return(
         <main>
