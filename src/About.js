07603ae67da382e027d1043051004a664132d6da
Irena Chen <irena1009@gmail.com>
2021-07-20 15:53:27 -0700

    build about.js

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './about.css'
+
+function About(){
+    return(
+        <main className="container">
+            <div className="avatar"></div>
+            <h1 className="name">Irena C</h1>
+            <h2 className="role">Full Stack Web Developer</h2>
+            <p className="paragraph1">I’m a full stack web developer devoting in helping small businesses to create the digital presence of their product/service and optimize work efficiency.</p>
+            <p className="paragraph2">I also specialized in business workflow optimization. By automating business workflow, your company operational efficiency can be improved by 50% in average. </p>
+            <p className="paragraph3">Connect with me to chat about it in details!</p>
+        </main>
+    )
+}
+
+export default About
