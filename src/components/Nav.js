9c2d995ecb4c3cd98eee576106c706a64eca9f8d
Irena Chen <irena1009@gmail.com>
2021-08-18 13:30:43 -0700

    styling Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Nav.css"
 
 function Nav() {
     return (
-        <div>
-            
+        <div className="navbar">
+            <div className="icon-wrapper">
+                <Link to='/about' className="icon">
+                    👋
+                </Link>
+                <Link to='/projects' className="icon">
+                    🗂
+                </Link>
+                <Link to='/contact' className="icon">
+                    📭
+                </Link>
+            </div>
+
         </div>
     )
 }
