f0cc44df2973354c0f2863398d508ffeee91c311
Irena Chen <irena1009@gmail.com>
2021-08-19 15:38:57 -0700

    create Card item component

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Card(props) {
+    return (
+        <>
+            <li className="card-item">
+                <a className="card-item-link" href={props.url}>
+                    <figure>
+                        <img src={props.imgDir} alt="" className="card-item-img"/>
+                    </figure>
+                    <div className="card-item-info">
+                        <h3>{props.subtitle}</h3>
+                        <p>{props.content}</p>
+                    </div>
+                </a>
+            </li>  
+        </>
+    )
+}
+
+export default Card
