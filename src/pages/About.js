998c13a8ba920121a1d95c8e188558ae2033bd0b
Irena Chen <irena1009@gmail.com>
2021-08-18 18:11:01 -0700

    center content

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,15 @@ import './About.css'
 
 function About(){
     return(
-        <main className="container">
-            <div className="avatar"></div>
-            <h1 className="name">Irena C</h1>
-            <h2 className="role">Full Stack Web Developer</h2>
-            <p className="paragraph1">I’m a full stack web developer devoting in helping small businesses to create the digital presence of their product/service and optimize work efficiency.</p>
-            <p className="paragraph2">I also specialized in business workflow optimization. By automating business workflow, your company operational efficiency can be improved by 50% in average. </p>
-            <p className="paragraph3">Connect with me to chat about it in details!</p>
+        <main>
+            <div className="container">
+                <div className="avatar"></div>
+                <h1 className="name">Irena C</h1>
+                <h2 className="role">Full Stack Web Developer</h2>
+                <p className="paragraph1">I’m a full stack web developer devoting in helping small businesses to create the digital presence of their product/service and optimize work efficiency.</p>
+                <p className="paragraph2">I also specialized in business workflow optimization. By automating business workflow, your company operational efficiency can be improved by 50% in average. </p>
+                <p className="paragraph3">Connect with me to chat about it in details!</p>
+            </div>
         </main>
     )
 }
