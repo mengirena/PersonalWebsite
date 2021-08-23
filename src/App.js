fca388435a8e2636bf8cd544715f2fb92f1ee946
Irena Chen <irena1009@gmail.com>
2021-08-22 23:21:29 -0700

    revise project filename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Irena from "./pages/Irena.js"
 import About from "./pages/About.js"
 import Navbar from "./components/Nav.js"
-import Projects from "./pages/Projects"
+import Projects from "./pages/Project"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
