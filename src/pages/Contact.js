3b25110272dfa91cb351d499ee8bc7e61baf745e
Irena Chen <irena1009@gmail.com>
2021-08-23 21:48:26 -0700

    add contact form styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import './Project.css'
+import './Contact.css'
 
 function Contact() {
     return (
         <main>
             <div className="container-project">
                 <h1 className="name">Get in Touch</h1>
-                <form name="contact personal" method="post" data-netlify="true" onSubmit="submit">
-                    <input type="hidden" name="form-name" value="contact personal" />
-                    <p>
-                        <label htmlFor="name">Your Name: </label>
-                        <input type="text" name="name"/>
-                    </p>
-                    <p>
-                        <label htmlFor="email">Your Email: </label>
-                        <input type="email" name="email"/>
-                    </p>
-                    <p>
-                        <label htmlFor="message">Message: </label>
-                        <textarea name="message"></textarea>
-                    </p>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
+                <section>
+                    <div className="left-form">
+                        <h3 className="role">Drop me a line...</h3>
+                        <form name="contact personal" method="post" data-netlify="true" onSubmit="submit">
+                            <input type="hidden" name="form-name" value="contact personal" />
+                            <div className="form-fill">
+                                <label htmKlFor="name">
+                                <input type="text" name="name" placeholder="Your Name"/></label>
+                                <label htmlFor="email">
+                                <input type="email" name="email" placeholder="Your Email"/></label>
+                                <label htmlFor="message">
+                                <textarea name="message" placeholder="Message for Me" style={{height:"10rem"}}></textarea></label>
+                            </div>
+                            <p>
+                                <button type="submit">Send</button>
+                            </p>
+                        </form>                                 
+                    </div>
+                    <div className="right-contact">
+                        
+                    </div>
+                </section>
+                
             </div>
         </main> 
     )
